1e602c2d6e9c0baacc77076e25181197d33e0276
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-08-05 23:21:43 +0300

    Add files via upload

---
+++ 8_Tuple.py
@@ -0,0 +1,21 @@
+#Tuple
+#1-
+print("1-: ")
+tuple_1=(1,5,6,3,4,7,3)
+print(type(tuple_1))
+print("Index number 7: ")
+print(tuple_1.index(7))
+print("1.index: ")
+print(tuple_1[1])
+print("How much 3: ")
+print(tuple_1.count(3))
+#2-
+print("2-: ")
+x="hello"
+tuple_2=tuple(x)
+print(tuple_2)
+
+
+
+
+
