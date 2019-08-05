e9cc4d217a99284977ab127297cf500dfcef6324
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-08-05 21:27:53 +0300

    Add files via upload

---
+++ 6_Print_Parameters.py
@@ -0,0 +1,19 @@
+#Parameters
+#1-
+print("1-: ")
+print("a","b","c","d")
+print(*"abcd")#param
+
+
+#2-
+print("2-: ")
+print("a","b","c","d",sep=".")
+print(*"abcd",sep=".")#param
+
+#3-
+print("3-: ")
+print("abcd.")
+print("abcd",end=".")#param
+
+
+
