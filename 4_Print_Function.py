49d05e4ecadd467b66a184b5df115ae6263a6913
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-08-05 19:45:11 +0300

    Add files via upload

---
+++ 4_Print_Function.py
@@ -0,0 +1,52 @@
+#Values
+a=1234 #integer(int) value
+b="abc" #string(str) value
+c=15.7 #Øfloat(float) value
+d="88888" #String(str) value
+
+
+#Print Values
+print("Print Values: ")
+print(a)
+print(b)
+print(c)
+
+#Values Transform
+print("Transform: ")
+print(float(a))
+print(int(c))
+print(str(a))
+
+#Print Function
+print("Print Function: ")
+print("a")
+print("wz"*3)
+print(a*c)
+
+e=20
+f=5
+print(e/f) # 1 "/"  float value
+print(e//f) # 2 "/" integer value
+print(e+f)
+print(e*f)
+print(e%f)
+print(e*f*3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
