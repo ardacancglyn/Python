6652b6e1b6e148b18dfc8850afb8799eba3b17e5
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-08-06 21:21:14 +0300

    Add files via upload

---
+++ 10_İnput_Function.py
@@ -0,0 +1,24 @@
+#İnput
+#1-String
+name=input("What is your name?: ")
+print(name)
+print(type(name))
+
+#2-İnt
+number=int(input("Your Favorite Number: "))
+print(number)
+print(type(number))
+
+#Simple Example
+movie=[]
+
+
+movie_name=input("Movie Name: ")
+imbd_point=float(input("İmbd Point: "))
+
+movie.append(movie_name)
+movie.append(imbd_point)
+print("Movie Name:  {}\n Movie İmbd Points: {}".format(movie_name,imbd_point))
+
+
+
