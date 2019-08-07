c9160fa40a0e69e74aefdd03c883fbcf62bac036
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-08-07 20:56:21 +0300

    Add files via upload

---
+++ 12_If_Elif_Else.py
@@ -0,0 +1,63 @@
+                                        #If-Elif-Else
+#Values
+x=int(input("Number-1: "))
+y=int(input("Number-2: "))
+
+
+#If-Elif-Else
+if x>y:
+    print("{}>{}".format(x,y))
+
+
+elif x<y:
+    print("{}<{}".format(x,y))
+
+else:
+    print("{}={}".format(y,x))
+#Same Code[Just else and elif different]
+"""
+if x>y:
+    print("{}>{}".format(x,y))
+
+
+elif x<y:
+    print("{}<{}".format(x,y))
+
+elif x==y:
+    print("{}={}".format(y,x))
+    
+"""
+#Same Code[Print code different]
+
+"""
+if x>y:
+    print("Number1>Number2")
+
+
+elif x<y:
+    print("Number1<Number2")
+
+else:
+    print("Number1>Number2")
+    
+"""
+
+#Odd and Even Number (Simple Example)
+a=int(input("Enter a number: "))
+if a % 2==0:
+    print("{} ".format(a) + "Even Number.")
+else:
+    print("{} ".format(a) + "Odd Number.")
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
