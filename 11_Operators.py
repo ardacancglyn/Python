dcd3f7aaa464b0e6bf31a51f66260c98164f1e74
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-08-07 13:56:47 +0300

    Add files via upload

---
+++ 11_Operators.py
@@ -0,0 +1,74 @@
+#Operators
+
+#1-)Comparison Operators
+"""
+*Operator    *Description	
+1-)   ==	 If the values of two operands are equal, then the condition becomes true.
+2-)   !=	 If values of two operands are not equal, then condition becomes true.	
+3-)   <>	 If values of two operands are not equal, then condition becomes true.	
+4-)   >	     If the value of left operand is greater than the value of right operand, then condition becomes true.	
+5-)   <	     If the value of left operand is less than the value of right operand, then condition becomes true.	
+6-)   >=	 If the value of left operand is greater than or equal to the value of right operand, then condition becomes true.	
+7-)   <=	If the value of left operand is less than or equal to the value of right operand, then condition becomes true.	
+
+*Example
+a=10,b=20 
+1-)    (a == b) is not true.
+2-)    (a != b) is true.
+3-)    (a <> b) is true. This is similar to != operator.
+4-)    (a > b) is not true.
+5-)    (a < b) is true.
+6-)    (a >= b) is not true.
+7-)    (a <= b) is true.
+
+"""
+
+
+
+
+#2-)Logical Operators
+"""
+1)a=true,b=false
+2)a=1>>True,b=0>>False
+
+
+*Operator                   *Description	
+1-)and Logical AND          If both the operands are true then condition becomes true.	
+2-)or Logical OR            If any of the two operands are non-zero then condition becomes true.	
+3-)not Logical NOT	        Used to reverse the logical state of its operand.	
+
+*Example
+1-)    (a and b) is true.
+2-)    (a or b) is true.
+3-)    Not(a and b) is false.
+
+"""
+
+
+
+#3-)Assignment Operators
+"""
+*Operator                     *Description	
+1-)  += Add AND	              It adds right operand to the left operand and assign the result to left operand	
+2-)  -= Subtract AND          It subtracts right operand from the left operand and assign the result to left operand	
+3-)  *= Multiply AND	      It multiplies right operand with the left operand and assign the result to left operand	
+4-)  /= Divide AND	          It divides left operand with the right operand and assign the result to left operand	
+5-)  %= Modulus AND	          It takes modulus using two operands and assign the result to left operand	
+6-)  **= Exponent AND	      Performs exponential (power) calculation on operators and assign value to the left operand	
+7-)  //= Floor Division	      It performs floor division on operators and assign value to the left operand	
+
+*Example
+1-) c += a is equivalent to c = c + a
+2-) c -= a is equivalent to c = c - a
+3-) c *= a is equivalent to c = c * a
+4-) c /= a is equivalent to c = c / ac /= a is equivalent to c = c / a
+5-) c %= a is equivalent to c = c % a
+6-) c **= a is equivalent to c = c ** a
+7-) c //= a is equivalent to c = c // a
+
+
+"""
+
+
+
+
