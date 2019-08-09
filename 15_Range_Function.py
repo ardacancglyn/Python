79a5e7a9cb306f4c458e4930b4c036b35cff4991
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-08-09 16:55:44 +0300

    Add files via upload

---
+++ 15_Range_Function.py
@@ -0,0 +1,48 @@
+#Range Function
+
+#1-)
+print("1-): ")
+a=range(20)
+
+print("Type: ")
+print(type(a))
+
+print(a)
+print(*a)
+
+
+
+
+#2-)Range to list
+print("2-): ")
+b=range(2,10) 
+print(*b)
+
+c=list(b)#b=range,c=b.list
+print(c)
+
+
+
+#3-)
+print("3-): ")
+d=range(0,50,2)
+print(*d)
+
+e=range(1,50,2)
+print(*e)
+
+#4-)
+print("4-): ")
+for i in range(20):
+    print(i)
+
+#5-)
+print("5-): ")
+for x in range(15):
+    print("*"*x)
+
+
+
+
+
+
