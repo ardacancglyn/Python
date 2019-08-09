100783a0094467604324ff777e54326a41bb7c29
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-08-09 15:55:07 +0300

    Add files via upload

---
+++ 14_While_Loop.py
@@ -0,0 +1,23 @@
+#While
+
+#1-)
+print("1-): ")
+x=10
+
+while(x>0):
+    print(x)
+    x-=1
+print("While Loops Done...")
+#2-)
+#For
+print("For Loop: ")
+lis1=[1,2,3,4,5,6,7,8,9,10]
+for i in lis1:
+    print(i)
+
+#While
+print("While Loop: ")
+index=0
+while(len(lis1)>index):
+    print(lis1[index])
+    index+=1
