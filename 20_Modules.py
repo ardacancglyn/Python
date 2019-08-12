bf4bbba98e9decea3e3acd80a164ade6b7861143
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-08-13 01:16:03 +0300

    Add files via upload

---
+++ 20_Modules.py
@@ -0,0 +1,25 @@
+#Modules
+import math # İmport Module
+import pandas as pd#İmport Library
+import numpy as np#İmport Library
+print(dir(math))
+
+log=math.log10(10)
+print(log)
+
+
+ceil=math.ceil(5.1)
+print(ceil)
+
+
+fac=math.factorial(5)
+print(fac)
+
+
+
+
+
+
+
+
+
