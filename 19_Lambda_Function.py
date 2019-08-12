26f9096ef253bfa380c941fce2fc1ac0af400a8f
Arda Can Çağlıyan <52882045+ardacancglyn@users.noreply.github.com>
2019-08-12 23:32:57 +0300

    Add files via upload

---
+++ 19_Lambda_Function.py
@@ -0,0 +1,17 @@
+#Lambda Function
+
+#1-)DEF (3 Line)
+print("DEF: ")
+def addition (x,y):
+    return (x+y)
+    
+
+print(addition(1,8))
+
+#2-)Lambda
+print("Lambda: ")
+adds=lambda x,y : x+y
+print(adds(5,7))
+
+
+
